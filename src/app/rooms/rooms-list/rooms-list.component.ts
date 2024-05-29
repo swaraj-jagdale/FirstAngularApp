126e86a8e986b632a31250c67b96e5a498cea57f
Jagdale, Swaraj <Swaraj_Jagdale@dell.com>
2024-05-29 11:31:23 +0530

    Day 2@office

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrl: './rooms-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomsListComponent {
+export class RoomsListComponent implements OnChanges{
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
   @Input() rooms: RoomList[] = [];
+  @Input() title: String = "";
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom.emit(room);
+  }
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -10,16 +10,20 @@ export class RoomsComponent {
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
   hideRooms = true;
+  selectedRoom!: RoomList;
   rooms: Room = {
     totalRooms: 20,
     availabeRooms: 10,
     bookedRooms: 5,
   };
 
+  title = 'Room List';
+
   roomList: RoomList[] = [];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+    this.title = 'Rooms List';
   }
 
   ngOnInit(): void {
@@ -59,5 +63,25 @@ export class RoomsComponent {
         rating: 2.7,
       },
     ];
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Presidential Suite',
+      ammenities: 'AC, TV, fridge, security and much more',
+      price: 75000,
+      photos:
+        'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+      checkinTime: new Date('01/09/2024'),
+      checkoutTime: new Date('02/09/2024'),
+      rating: 5,
+    };
+    // this.roomList.push(room);
+    this.roomList = [...this.roomList, room];
   }
 }
